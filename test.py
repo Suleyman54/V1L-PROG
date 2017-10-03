3925eaf44aa47349f49abe41ff3dec282efa809c
Süleyman <suleyman.altindag@student.hu.nl>
2017-10-03 15:24:05 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,48 @@
+# a = 'i\'m süleyman, "what is you name?"'
+# print(a)
+
+# poem = '''
+# To make a prairie it takes a clover and one bee, -
+# One clover, and a bee,
+# And revery.
+# The revery alone will do
+# If bees are few.
+# '''
+# print(poem)
+
+# a = 'jebentdik'
+# print(a[0:6])
+
+# message = '''This message is top secret and should not
+# be divulged to anyone without top secret clearance'''
+# print(message.find('top secret'))
+
+# message = '''This message is top secret and should not
+# be divulged to anyone without top secret clearance'''
+# print(message.count('top secret'))
+
+# s = 'hallo iedereen. hoe gaat het met iedereen'
+# print(s.capitalize())
+
+# s = 'hallo iedereen. hoe gaat het met iedereen'
+# print(s.upper())
+
+# a = 'heey ik ben suleyman'
+# print(a.split())
+
+# x = '2;3;5;7;11;13'
+# print(x.split(';'))
+#
+# for name in ['Joe', 'Sam', 'Tim', 'Ann']:
+#     print(name)
+
+# weekday = 'Wednesday'
+# month = 'March'
+# day = 10
+# year = 2010
+# hour = 11
+# minute = 45
+# second = 33
+#
+# print(weekday+', '+month+' '+str(day)+', '+str(year)
+# +' at '+str(hour)+':'+str(minute)+':'+str(second))
